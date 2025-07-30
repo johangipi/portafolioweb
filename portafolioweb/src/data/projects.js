c2a0e930fdd030440c2508747370ebf85f8e3ea9
Johangipi <johangiraldo239@gmail.com>
2025-07-29 19:19:59 -0500

    confirmacion de datos

--- portafolioweb/src/data/projects.js
+++ portafolioweb/src/data/projects.js
@@ -2,21 +2,21 @@ const projects = [
   {
     title: "HandLogistic",
     description: "CRUD de gestión de logística para inventarios y pedidos.",
-    link: "https://github.com/tuusuario/proyecto",
+    link: "https://github.com/johangipi/HandLogistic1.2.1",
     technologies: ["HTML", "CSS", "PHP", "JavaScript", "MySQL"],
   },
   {
     title: "Portafolio Web",
     description: "Sitio web personal para mostrar proyectos y habilidades.",
-    link: "https://github.com/tuusuario/portafolio",
+    link: "https://github.com/johangipi",
     technologies: ["React", "CSS", "Vite"],
   },
 
   {
     title: "Practicas de Programación",
     description: "Repositorio de prácticas de programación en JavaScript.",
-    link: "https://github.com/tuusuario/portafolio",
-    technologies: ["React", "CSS", "Vite"],
+    link: "https://github.com/johangipi/proyectos",
+    technologies: ["PHP", "CSS", "HTML"],
   },
   // ...otros proyectos
 ];
